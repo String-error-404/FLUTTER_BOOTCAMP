3774c07269c1e96787e02ed979d53d267763606d
Vishal-Adhithya <vishaladhithya19@gmail.com>
2020-07-06 20:31:39 +0530

    i am rich

---
+++ The Complete 2020 Flutter Development Bootcamp with Dart/i_am_rich/android/app/src/main/kotlin/vishaladhithya/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package vishaladhithya.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ The Complete 2020 Flutter Development Bootcamp with Dart/i_am_rich/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+// this can be done with => or {wrapping up the code inside}
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('hi world'),
+      ),
+    ),
+  );
+}
+
+// hot reload;
+
+
