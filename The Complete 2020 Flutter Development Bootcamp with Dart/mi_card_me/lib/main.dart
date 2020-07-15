ad61793d64b89bcf06e8c2a630c814c97fe6a1d6
Vishal-Adhithya <vishaladhithya19@gmail.com>
2020-07-15 22:04:58 +0530

    simple mi card

--- The Complete 2020 Flutter Development Bootcamp with Dart/mi_card_me/lib/main.dart
+++ The Complete 2020 Flutter Development Bootcamp with Dart/mi_card_me/lib/main.dart
@@ -40,6 +40,52 @@ class MyApp extends StatelessWidget {
                   ),
                 ),
               ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    '+144 123 456 789',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      color: Colors.teal.shade900,
+                      fontSize: 20,
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.developer_mode,
+                  ),
+                ),
+              ),
+              Card(
+                // ! if you rap with the card widget a padding class it creates a blank space....
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                color: Colors.white,
+                // * the card property comes with default white color.. you can delete without any doubt
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text(
+                    'vishal19@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      color: Colors.teal.shade900,
+                      fontSize: 20,
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.alternate_email,
+                  ),
+                  dense: true,
+                  onTap: () => TapUpDetails,
+                ),
+              ),
             ],
           ),
         ),
@@ -47,3 +93,51 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+// TODO finished the projects ::))
+
+// ! before list tile...????
+// Padding(
+//                   padding: const EdgeInsets.all(25.0),
+//                   child: Row(
+//                     // *card property doesn't have padding property....
+//                     children: <Widget>[
+//                       Icon(
+//                         Icons.email,
+//                         color: Colors.teal.shade900,
+//                       ),
+//                       SizedBox(
+//                         width: 10,
+//                       ),
+//                       Text(
+//                         'vishaladhithya19@gmail.com',
+//                         style: TextStyle(
+//                           fontFamily: 'SourceSansPro',
+//                           color: Colors.teal.shade900,
+//                           fontSize: 20,
+//                         ),
+//                       )
+//                     ],
+//                   ),
+//                 ),
+
+// ! number >>>>> without listtile....
+
+// Row(
+//                   children: <Widget>[
+//                     Icon(
+//                       Icons.phone,
+//                       color: Colors.teal.shade900,
+//                     ),
+//                     SizedBox(
+//                       width: 10,
+//                     ),
+//                     Text(
+//                       '+144 123 456 789',
+//                       style: TextStyle(
+//                         fontFamily: 'SourceSansPro',
+//                         color: Colors.teal.shade900,
+//                         fontSize: 20,
+//                       ),
+//                     )
+//                   ],
+//                 ),
